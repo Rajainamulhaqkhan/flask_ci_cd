46445297f94fec08ff647e2943f0c9e2cfbc37fe
Raja Inam Ul Haq Khan <inam.haq@contour-software.com>
2026-02-12 20:22:53 +0500

    Mock DB connection for CI tests

--- test_app.py
+++ test_app.py
@@ -1,6 +1,10 @@
 from app import app
+from unittest.mock import patch, MagicMock
 
 def test_home():
-    client = app.test_client()
-    response = client.get('/')
-    assert response.status_code == 200
+    with patch("app.get_db_connection") as mock_conn:
+        mock_conn.return_value = MagicMock()
+        client = app.test_client()
+        response = client.get('/')
+        assert response.status_code == 200
+        assert b"task 4 completed" in response.data
